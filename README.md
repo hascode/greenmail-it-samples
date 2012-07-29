e932fd3a1cbf87e59c9d13f0b0931643d5484717
hascode <hascode@localhost>
2012-07-29 20:41:51 +0200

    Initial import.

---
+++ README.md
@@ -0,0 +1,12 @@
+# E-Mail Integration Testing with Greenmail Examples
+
+
+Examples how to run integration tests for SMTP, POP3, IMAP, IMAPS.. using the GreenMail library.
+
+For the full tutorial please feel free to visit my blog at [www.hascode.com].
+
+---
+
+*2012 Micha Kops / hasCode.com*
+
+  [www.hascode.com]:http://www.hascode.com/
---
+++ src/test/java/it/Pop3IT.java
@@ -0,0 +1,83 @@
+package it;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Store;
+import javax.mail.URLName;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.icegreen.greenmail.user.GreenMailUser;
+import com.icegreen.greenmail.user.UserException;
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetupTest;
+
+public class Pop3IT {
+	private static final String USER_PASSWORD = "abcdef123";
+	private static final String USER_NAME = "hascode";
+	private static final String EMAIL_USER_ADDRESS = "hascode@localhost";
+	private static final String EMAIL_TO = "someone@localhost.com";
+	private static final String EMAIL_SUBJECT = "Test E-Mail";
+	private static final String EMAIL_TEXT = "This is a test e-mail.";
+	private static final String LOCALHOST = "127.0.0.1";
+	private GreenMail mailServer;
+
+	@Before
+	public void setUp() {
+		mailServer = new GreenMail(ServerSetupTest.POP3);
+		mailServer.start();
+	}
+
+	@After
+	public void tearDown() {
+		mailServer.stop();
+	}
+
+	@Test
+	public void getMails() throws IOException, MessagingException,
+			UserException, InterruptedException {
+		GreenMailUser user = mailServer.setUser(EMAIL_USER_ADDRESS, USER_NAME,
+				USER_PASSWORD);
+		MimeMessage message = new MimeMessage((Session) null);
+		message.setFrom(new InternetAddress(EMAIL_TO));
+		message.addRecipient(Message.RecipientType.TO, new InternetAddress(
+				EMAIL_USER_ADDRESS));
+		message.setSubject(EMAIL_SUBJECT);
+		message.setText(EMAIL_TEXT);
+		user.deliver(message);
+
+		Properties props = new Properties();
+		props.setProperty("mail.pop3.connectiontimeout", "5000");
+		Session session = Session.getInstance(props);
+		URLName urlName = new URLName("pop3", LOCALHOST,
+				ServerSetupTest.POP3.getPort(), null, user.getLogin(),
+				user.getPassword());
+		Store store = session.getStore(urlName);
+		store.connect();
+
+		Folder folder = store.getFolder("INBOX");
+		folder.open(Folder.READ_ONLY);
+		Message[] messages = folder.getMessages();
+		assertNotNull(messages);
+		assertThat(1, equalTo(messages.length));
+		assertEquals(EMAIL_SUBJECT, messages[0].getSubject());
+		assertTrue(String.valueOf(messages[0].getContent())
+				.contains(EMAIL_TEXT));
+		assertEquals(EMAIL_TO, messages[0].getFrom()[0].toString());
+	}
+}
